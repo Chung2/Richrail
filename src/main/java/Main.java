07ecf1ebbd1b3ae24ecf141b3a264c43f11bb1e5
Nickernator <nickwindt@hotmail.nl>
2017-11-30 11:14:14 +0100

    Commit #5, fixed an issue in the Main class. Improved the readability of the code to binary

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@
  */
 public class Main {
     public static void main(String [] args){
-        System.out.println("H3ll0 W0rld!!!!");
+        System.out.println("010010000011001101101100011011000011000000100000010101110011000001110010011011000110010000100001001000010010000100100001");
     }
 }
