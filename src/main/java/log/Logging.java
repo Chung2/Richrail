2c20ffd0c8b28e8cd7db1517a13322793848d594
Nickernator <nickwindt@hotmail.nl>
2017-12-14 11:38:34 +0100

    Attempt to fix Logging error, replaced "/" with File.seperator

--- src/main/java/log/Logging.java
+++ src/main/java/log/Logging.java
@@ -34,10 +34,11 @@ public class Logging {
             String filename = getCurrentTime("dd-MM-yyyy_HHmmss") + ".txt";
 
             // create directories if they dont exist already
-            new File ("src/log/"+getCurrentTime("dd-MM-yyyy")).mkdirs();
+            new File ("src"+File.separator+"log"+File.separator+getCurrentTime("dd-MM-yyyy")).mkdirs();
+
 
             // create the file
-            File file = new File("src/log/" + getCurrentTime("dd-MM-yyyy") + "/" + filename);
+            File file = new File("src"+File.separator+"log"+File.separator+getCurrentTime("dd-MM-yyyy") +File.separator+ filename);
             setPrintWriter(new PrintWriter(file));
 
             // write a few lines
